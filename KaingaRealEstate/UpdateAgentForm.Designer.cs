50d3bb64d57f98c808938d1fa47f51ff32d2631c
aliceypuff <alice.turnbull@live.com>
2022-11-10 00:59:12 +1300

    Update Agent looking okay, still need data entry restriction exceptions on all

--- KaingaRealEstate/UpdateAgentForm.Designer.cs
+++ KaingaRealEstate/UpdateAgentForm.Designer.cs
@@ -44,6 +44,24 @@ namespace KaingaRealEstate
             this.btnReturn = new System.Windows.Forms.Button();
             this.btnUpdateAgent = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.lblSA = new System.Windows.Forms.Label();
+            this.txtSuburb = new System.Windows.Forms.TextBox();
+            this.lblSuburb = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.nudSalary = new System.Windows.Forms.NumericUpDown();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.cboCert = new System.Windows.Forms.ComboBox();
+            this.lblCert = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buyercategoryTableAdapter1 = new KaingaRealEstate.KaingaRealEstateDataSetTableAdapters.BUYERCATEGORYTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSalary)).BeginInit();
             this.SuspendLayout();
             // 
             // lblrequired2
@@ -52,7 +70,7 @@ namespace KaingaRealEstate
             this.lblrequired2.BackColor = System.Drawing.Color.Transparent;
             this.lblrequired2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblrequired2.ForeColor = System.Drawing.Color.Red;
-            this.lblrequired2.Location = new System.Drawing.Point(180, 191);
+            this.lblrequired2.Location = new System.Drawing.Point(180, 161);
             this.lblrequired2.Margin = new System.Windows.Forms.Padding(0);
             this.lblrequired2.Name = "lblrequired2";
             this.lblrequired2.Size = new System.Drawing.Size(17, 19);
@@ -64,7 +82,7 @@ namespace KaingaRealEstate
             this.lblrequired1.AutoSize = true;
             this.lblrequired1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblrequired1.ForeColor = System.Drawing.Color.Red;
-            this.lblrequired1.Location = new System.Drawing.Point(180, 239);
+            this.lblrequired1.Location = new System.Drawing.Point(180, 194);
             this.lblrequired1.Name = "lblrequired1";
             this.lblrequired1.Size = new System.Drawing.Size(17, 19);
             this.lblrequired1.TabIndex = 72;
@@ -86,7 +104,7 @@ namespace KaingaRealEstate
             this.lblInstruction2.AutoSize = true;
             this.lblInstruction2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblInstruction2.ForeColor = System.Drawing.Color.Red;
-            this.lblInstruction2.Location = new System.Drawing.Point(204, 435);
+            this.lblInstruction2.Location = new System.Drawing.Point(204, 369);
             this.lblInstruction2.Name = "lblInstruction2";
             this.lblInstruction2.Size = new System.Drawing.Size(147, 19);
             this.lblInstruction2.TabIndex = 70;
@@ -95,7 +113,7 @@ namespace KaingaRealEstate
             // txtFirstName
             // 
             this.txtFirstName.BackColor = System.Drawing.SystemColors.Window;
-            this.txtFirstName.Location = new System.Drawing.Point(199, 247);
+            this.txtFirstName.Location = new System.Drawing.Point(199, 202);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(272, 27);
             this.txtFirstName.TabIndex = 69;
@@ -103,14 +121,14 @@ namespace KaingaRealEstate
             // txtLastName
             // 
             this.txtLastName.BackColor = System.Drawing.SystemColors.Window;
-            this.txtLastName.Location = new System.Drawing.Point(199, 199);
+            this.txtLastName.Location = new System.Drawing.Point(199, 169);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(272, 27);
             this.txtLastName.TabIndex = 68;
             // 
             // txtAgentID
             // 
-            this.txtAgentID.Location = new System.Drawing.Point(199, 151);
+            this.txtAgentID.Location = new System.Drawing.Point(199, 136);
             this.txtAgentID.Name = "txtAgentID";
             this.txtAgentID.ReadOnly = true;
             this.txtAgentID.Size = new System.Drawing.Size(79, 27);
@@ -128,7 +146,7 @@ namespace KaingaRealEstate
             // lblFirstName
             // 
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(110, 250);
+            this.lblFirstName.Location = new System.Drawing.Point(110, 205);
             this.lblFirstName.Name = "lblFirstName";
             this.lblFirstName.Size = new System.Drawing.Size(83, 19);
             this.lblFirstName.TabIndex = 65;
@@ -138,7 +156,7 @@ namespace KaingaRealEstate
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(111, 202);
+            this.lblLastName.Location = new System.Drawing.Point(111, 172);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.Size = new System.Drawing.Size(82, 19);
             this.lblLastName.TabIndex = 64;
@@ -148,7 +166,7 @@ namespace KaingaRealEstate
             // lblAgentD
             // 
             this.lblAgentD.AutoSize = true;
-            this.lblAgentD.Location = new System.Drawing.Point(124, 154);
+            this.lblAgentD.Location = new System.Drawing.Point(124, 139);
             this.lblAgentD.Name = "lblAgentD";
             this.lblAgentD.Size = new System.Drawing.Size(69, 19);
             this.lblAgentD.TabIndex = 63;
@@ -167,7 +185,7 @@ namespace KaingaRealEstate
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(454, 469);
+            this.btnReturn.Location = new System.Drawing.Point(454, 403);
             this.btnReturn.Name = "btnReturn";
             this.btnReturn.Size = new System.Drawing.Size(231, 48);
             this.btnReturn.TabIndex = 61;
@@ -177,7 +195,7 @@ namespace KaingaRealEstate
             // 
             // btnUpdateAgent
             // 
-            this.btnUpdateAgent.Location = new System.Drawing.Point(69, 469);
+            this.btnUpdateAgent.Location = new System.Drawing.Point(69, 403);
             this.btnUpdateAgent.Name = "btnUpdateAgent";
             this.btnUpdateAgent.Size = new System.Drawing.Size(231, 48);
             this.btnUpdateAgent.TabIndex = 60;
@@ -195,11 +213,204 @@ namespace KaingaRealEstate
             this.lblTitle.TabIndex = 59;
             this.lblTitle.Text = "Update Agent";
             // 
+            // txtStreetAddress
+            // 
+            this.txtStreetAddress.BackColor = System.Drawing.SystemColors.Window;
+            this.txtStreetAddress.Location = new System.Drawing.Point(199, 235);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(486, 27);
+            this.txtStreetAddress.TabIndex = 74;
+            // 
+            // lblSA
+            // 
+            this.lblSA.AutoSize = true;
+            this.lblSA.Location = new System.Drawing.Point(86, 238);
+            this.lblSA.Name = "lblSA";
+            this.lblSA.Size = new System.Drawing.Size(107, 19);
+            this.lblSA.TabIndex = 75;
+            this.lblSA.Text = "Street Address:";
+            this.lblSA.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtSuburb
+            // 
+            this.txtSuburb.BackColor = System.Drawing.SystemColors.Window;
+            this.txtSuburb.Location = new System.Drawing.Point(199, 268);
+            this.txtSuburb.Name = "txtSuburb";
+            this.txtSuburb.Size = new System.Drawing.Size(245, 27);
+            this.txtSuburb.TabIndex = 76;
+            // 
+            // lblSuburb
+            // 
+            this.lblSuburb.AutoSize = true;
+            this.lblSuburb.Location = new System.Drawing.Point(136, 271);
+            this.lblSuburb.Name = "lblSuburb";
+            this.lblSuburb.Size = new System.Drawing.Size(57, 19);
+            this.lblSuburb.TabIndex = 77;
+            this.lblSuburb.Text = "Suburb:";
+            this.lblSuburb.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.Location = new System.Drawing.Point(504, 271);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(52, 19);
+            this.lblSalary.TabIndex = 79;
+            this.lblSalary.Text = "Salary:";
+            this.lblSalary.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nudSalary
+            // 
+            this.nudSalary.Location = new System.Drawing.Point(562, 268);
+            this.nudSalary.Maximum = new decimal(new int[] {
+            20000000,
+            0,
+            0,
+            131072});
+            this.nudSalary.Minimum = new decimal(new int[] {
+            7000000,
+            0,
+            0,
+            131072});
+            this.nudSalary.Name = "nudSalary";
+            this.nudSalary.Size = new System.Drawing.Size(123, 27);
+            this.nudSalary.TabIndex = 80;
+            this.nudSalary.Value = new decimal(new int[] {
+            7000000,
+            0,
+            0,
+            131072});
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.SystemColors.Window;
+            this.txtEmail.Location = new System.Drawing.Point(199, 301);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(284, 27);
+            this.txtEmail.TabIndex = 81;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(88, 304);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(105, 19);
+            this.lblEmail.TabIndex = 82;
+            this.lblEmail.Text = "Email Address:";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPhone.Location = new System.Drawing.Point(199, 334);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(123, 27);
+            this.txtPhone.TabIndex = 83;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(86, 337);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(108, 19);
+            this.lblPhone.TabIndex = 84;
+            this.lblPhone.Text = "Phone Number:";
+            this.lblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cboCert
+            // 
+            this.cboCert.FormattingEnabled = true;
+            this.cboCert.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.cboCert.Location = new System.Drawing.Point(631, 304);
+            this.cboCert.Name = "cboCert";
+            this.cboCert.Size = new System.Drawing.Size(54, 27);
+            this.cboCert.TabIndex = 85;
+            // 
+            // lblCert
+            // 
+            this.lblCert.AutoSize = true;
+            this.lblCert.Location = new System.Drawing.Point(533, 307);
+            this.lblCert.Name = "lblCert";
+            this.lblCert.Size = new System.Drawing.Size(92, 19);
+            this.lblCert.TabIndex = 86;
+            this.lblCert.Text = "Certification:";
+            this.lblCert.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(180, 227);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 19);
+            this.label1.TabIndex = 88;
+            this.label1.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(180, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 19);
+            this.label2.TabIndex = 87;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(180, 293);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 19);
+            this.label3.TabIndex = 90;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(180, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 19);
+            this.label4.TabIndex = 89;
+            this.label4.Text = "*";
+            // 
+            // buyercategoryTableAdapter1
+            // 
+            this.buyercategoryTableAdapter1.ClearBeforeFill = true;
+            // 
             // UpdateAgentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 561);
+            this.ClientSize = new System.Drawing.Size(762, 497);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCert);
+            this.Controls.Add(this.cboCert);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.nudSalary);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblSuburb);
+            this.Controls.Add(this.txtSuburb);
+            this.Controls.Add(this.lblSA);
+            this.Controls.Add(this.txtStreetAddress);
             this.Controls.Add(this.lblrequired2);
             this.Controls.Add(this.lblrequired1);
             this.Controls.Add(this.lblInstruction1);
@@ -219,6 +430,8 @@ namespace KaingaRealEstate
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UpdateAgentForm";
             this.Text = "Update Agents";
+            this.Load += new System.EventHandler(this.UpdateAgentForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSalary)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -241,5 +454,22 @@ namespace KaingaRealEstate
         private System.Windows.Forms.Button btnReturn;
         private System.Windows.Forms.Button btnUpdateAgent;
         private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.Label lblSA;
+        private System.Windows.Forms.TextBox txtSuburb;
+        private System.Windows.Forms.Label lblSuburb;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.NumericUpDown nudSalary;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.ComboBox cboCert;
+        private System.Windows.Forms.Label lblCert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private KaingaRealEstateDataSetTableAdapters.BUYERCATEGORYTableAdapter buyercategoryTableAdapter1;
     }
 }
--- KaingaRealEstate/UpdateAgentForm.cs
+++ KaingaRealEstate/UpdateAgentForm.cs
@@ -36,68 +36,93 @@ namespace KaingaRealEstate
             txtAgentID.Text = "";
             txtLastName.Text = "";
             txtFirstName.Text = "";
+            txtStreetAddress.Text = "";
+            txtSuburb.Text = "";
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+            nudSalary.ResetText();
+            cboCert.Text = "";
+            cboCert.Items.Clear();
             cboAgent.Text = "";
             cboAgent.Items.Clear();
         }
-        private void LoadSuburbs()
+        private void LoadAgents()
         {
-            foreach (DataRow drSuburb in DC.dtSuburb.Rows)
+            foreach (DataRow drAgent in DC.dtAgent.Rows)
             {
-                cboAgent.Items.Add(drSuburb["suburbID"] + (" ") + drSuburb["suburbName"] + (" ") + drSuburb["postcode"]);
+                cboAgent.Items.Add(drAgent["agentID"] + (" ") + drAgent["lastName"] + (" ") + drAgent["firstName"]);
             }
         }
 
         private void cboAgent_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string suburb;
+            string agent;
 
-            if (cboSuburb.SelectedItem != null)
+            if (cboAgent.SelectedItem != null)
             {
-                // get the selected suburb
-                suburb = cboSuburb.SelectedItem.ToString();
+                // get the selected agent
+                agent = cboAgent.SelectedItem.ToString();
                 // split the string from cbo
-                string[] parts = suburb.Split(' ');
+                string[] parts = agent.Split(' ');
                 // extract ID
-                aSuburbID = Convert.ToInt32(parts[0]);
-                // Get the position of the selected suburb and assigns it to the Position property of cmSuburb
-                cmSuburb.Position = DC.suburbView.Find(aSuburbID);
-                // Get the datarow for the selected suburb
-                DataRow drSuburb = DC.dtSuburb.Rows[cmSuburb.Position];
-                // Populate suburb textboxes
-                txtAgentID.Text = drSuburb["suburbID"].ToString();
-                txtLastName.Text = drSuburb["suburbName"].ToString();
-                txtFirstName.Text = drSuburb["postcode"].ToString();
+                aAgentID = Convert.ToInt32(parts[0]);
+                // Get the position of the selected agent and assigns it to the Position property of cmAgent
+                cmAgent.Position = DC.suburbView.Find(aAgentID);
+                // Get the datarow for the selected agent
+                DataRow drAgent = DC.dtAgent.Rows[cmAgent.Position];
+                // Populate agent textboxes
+                txtAgentID.Text = drAgent["agentID"].ToString();
+                txtLastName.Text = drAgent["lastName"].ToString();
+                txtFirstName.Text = drAgent["firstName"].ToString();
+                txtStreetAddress.Text = drAgent["streetAddress"].ToString();
+                txtSuburb.Text = drAgent["suburb"].ToString();
+                txtEmail.Text = drAgent["emailAddress"].ToString();
+                txtPhone.Text = drAgent["phoneNumber"].ToString();
+                nudSalary.Text = drAgent["salary"].ToString();
+                cboCert.Text = drAgent["certification"].ToString();
             }
         }
         private void btnUpdateAgent_Click(object sender, EventArgs e)
         {
-            if (cboSuburb.SelectedIndex == 0) // cannot click it if the suburb is not selected 
+            if (cboAgent.SelectedIndex == 0) // cannot click it if the agent is not selected 
             {
-                // handle case where no suburb has been selected
-                MessageBox.Show("A suburb hasn't been selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // handle case where no agent has been selected
+                MessageBox.Show("An agent hasn't been selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else if (txtAgentID.Text == "" || txtLastName.Text == "" || txtFirstName.Text == "")
+            else if (txtAgentID.Text == "" || txtLastName.Text == "" || txtFirstName.Text == "" || txtStreetAddress.Text == "" || txtSuburb.Text == "" || txtEmail.Text == "" || txtPhone.Text == "")
             {
                 // handle cases where any of the input fields are empty
                 MessageBox.Show("One or more field is blank", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-                // get data row for selected suburb
-                DataRow updateSuburbRow = DC.dtSuburb.Rows[cmSuburb.Position];
-                // assign textbox values to updateSuburbRow
-                updateSuburbRow["suburbName"] = txtLastName.Text;
-                updateSuburbRow["postcode"] = txtFirstName.Text;
+                // get data row for selected agent
+                DataRow updateAgentRow = DC.dtAgent.Rows[cmAgent.Position];
+                // assign textbox values to updateAgentRow
+                updateAgentRow["lastName"] = txtLastName.Text;
+                updateAgentRow["firstName"] = txtFirstName.Text;
+                updateAgentRow["streetAddress"] = txtStreetAddress.Text;
+                updateAgentRow["suburb"] = txtSuburb.Text;
+                updateAgentRow["emailAddress"] = txtEmail.Text;
+                updateAgentRow["phoneNumber"] = txtPhone.Text;
+                updateAgentRow["salary"] = nudSalary.Text;
+                updateAgentRow["certification"] = cboCert.Text;
                 // End current edit if confirmed
-                if (MessageBox.Show("Are you sure you want to update this suburb?", "Warning", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                if (MessageBox.Show("Are you sure you want to update this agent?", "Warning", MessageBoxButtons.YesNo) == DialogResult.Yes)
                 {
-                    cmSuburb.EndCurrentEdit();
-                    DC.UpdateSuburb();
-                    MessageBox.Show("Suburb updated successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    cmAgent.EndCurrentEdit();
+                    DC.UpdateAgent();
+                    MessageBox.Show("Agent updated successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     ClearFields();
-                    LoadSuburbs();
+                    LoadAgents();
                 }
             }
+        }
+
+        private void UpdateAgentForm_Load(object sender, EventArgs e)
+        {
+            ClearFields();
+            LoadAgents();
         }
     }
 
